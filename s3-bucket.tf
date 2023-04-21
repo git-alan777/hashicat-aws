acd63ddfee98709da1d02a51f4270163d3af2123
git-alan777 <alantony25@gmail.com>
2023-04-21 10:06:15 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,3 +1,7 @@
+credentials "app.terraform.io" {
+    # valid user API token:
+    token = "C88Lx7NhypIK1A.atlasv1.FE7EqUyR8vlhjC5lUKgshAFA4RpB2WZK1zaPxeWXZ8asBxpHgEnRfeqc0bJexennOAA"
+  }
 module "s3-bucket" {
   source  = "app.terraform.io/example-org-0f84f7/s3-bucket/aws"
   version = "2.8.0"
