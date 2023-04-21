1c0b9c35f955e437aa9379aad0534e2da5c0ec09
git-alan777 <alantony25@gmail.com>
2023-04-21 09:53:01 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+module "s3-bucket" {
+  source  = "app.terraform.io/example-org-0f84f7/s3-bucket/aws"
+  version = "2.8.0"
   bucket = "my-s3-bucket"
   acl    = "private"
   bucket_prefix = "alantony"
