063d73e5f5c31b520e69752ced14257a7eb4c1b1
git-alan777 <alantony25@gmail.com>
2023-04-21 10:40:11 +0000

    Added s3 bucket module

--- variables.tf
+++ variables.tf
@@ -4,7 +4,12 @@
 # Here is where we store the default values for all the variables used in our
 # Terraform code. If you create a variable with no default, the user will be
 # prompted to enter it (or define it via config file or command line flags.)
-
+variable "bucket" {
+  default = my-s3-bucket
+}
+variable "bucket_prefix" {
+  default = alantony
+}
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
 }
